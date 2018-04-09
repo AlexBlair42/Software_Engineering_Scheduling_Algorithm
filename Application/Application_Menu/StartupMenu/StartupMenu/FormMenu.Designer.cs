0f7964094aa9218c9d3efd4f19a0ff340ad2e6d8
Chris <cblair-garcia18@my.whitworth.edu>
2018-04-08 19:26:43 -0700

    Import Button links to Excel loader project.

--- Application/Application_Menu/StartupMenu/StartupMenu/FormMenu.Designer.cs
+++ Application/Application_Menu/StartupMenu/StartupMenu/FormMenu.Designer.cs
@@ -31,11 +31,13 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(505, 68);
+            this.button1.Location = new System.Drawing.Point(459, 160);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(248, 56);
             this.button1.TabIndex = 0;
@@ -45,7 +47,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(505, 166);
+            this.button2.Location = new System.Drawing.Point(459, 235);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(248, 56);
             this.button2.TabIndex = 1;
@@ -55,7 +57,7 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(505, 263);
+            this.button3.Location = new System.Drawing.Point(459, 310);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(248, 56);
             this.button3.TabIndex = 2;
@@ -63,16 +65,26 @@
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.btnMaster_Click);
             // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1253, 503);
+            this.panel1.TabIndex = 3;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panelMaster_Paint);
+            // 
             // FormMenu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1277, 527);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
             this.Name = "FormMenu";
             this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -82,6 +94,7 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
--- Application/Application_Menu/StartupMenu/StartupMenu/FormMenu.cs
+++ Application/Application_Menu/StartupMenu/StartupMenu/FormMenu.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.IO;
 using System.Windows.Forms;
 
 namespace StartupMenu
 {
+
     public partial class FormMenu : Form
     {
         public FormMenu()
@@ -10,19 +12,25 @@ namespace StartupMenu
             InitializeComponent();
         }
 
-        private void btnImport_Click(object sender, EventArgs e)
+       private void btnImport_Click(object sender, EventArgs e)
         {
-            //opens the loader form
+           //opens the loader form
             Excel_Loader_Application.Form1 Import = new Excel_Loader_Application.Form1();
             Import.Show();
         }
 
         private void btnDirectory_Click(object sender, EventArgs e)
         {
+            System.Diagnostics.Process.Start("c:\\");
 
         }
 
         private void btnMaster_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panelMaster_Paint(object sender, PaintEventArgs e)
         {
 
         }
