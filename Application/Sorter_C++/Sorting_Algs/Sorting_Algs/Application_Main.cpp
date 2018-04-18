efdfa4ff178367eca114c893ab554a169352b4d3
Alex Blair <ablair18@my.whitworth.edu>
2018-04-17 21:14:50 -0700

    Organize Repo and merge projects
    Organized the repository and solved merge conflicts by manually merging to master.

---
+++ Application/Sorter_C++/Sorting_Algs/Sorting_Algs/Application_Main.cpp
@@ -0,0 +1,12 @@
+// This is the main C++ function for the sorting algs
+// Alex Blair
+// 4/17/18
+#include <iostream>
+using std::endl;
+using std::cout;
+using std::cin;
+
+int main()
+{
+	std::cin.get();
+}
---
+++ Application/Sorter_C++/Sorting_Algs/Sorting_Algs/Merge_Sort.cpp
@@ -0,0 +1,6 @@
+
+
+void Merge_Sort()
+{
+	int Lunch_END = 
+}
