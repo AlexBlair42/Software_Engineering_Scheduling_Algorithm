ffc0ea93f80eb7215aba10adbe37a99a1b33f982
Humanphyti <humanphyti@gmail.com>
2018-04-26 15:44:34 -0700

    more edits to the placement algorithm

--- Application/C++_Sorting/PlacementAlgorithm/ConsoleApplication4/FillWeek.cpp
+++ Application/C++_Sorting/PlacementAlgorithm/ConsoleApplication4/FillWeek.cpp
@@ -1,37 +1,66 @@
 #include "FillWeek.h"
 
-void Subject::populatedArrays(int subjectWeek[65], int teacherArray[18])
+void Subject::populatedArrays(int subjectWeek[65], int teacherArray[18][2])
 {
 	//fill subjectweek array with 0s
 	for (int i = 0; i < 65; i++) {
 		subjectWeek[i] = 0;
 		//fill the teacher array with the numerical values of teachers
 		if (i < 18) {
-			teacherArray[i] = i;
+			teacherArray[i][i] = i;
 		}
 	}
 }
 
-void Subject::placeTeachers(int (&week)[65], int (&teacher)[18])
+void Subject::placeTeachers(int (&week)[65], int (&teacher)[18][2], teacherArray[18][2])
 {
 	for (int t = 0; t < 18; t++) {
 		
 		for (int s = 0; s < 65; s++) {
-			if (s < 13 && !totaledHour(week, teacher, 1)) {
-				week[s] = teacher[t];
+			if (s < 13 && !totaledHour(week, teacher[t][2], 1, )) {
+				week[s] = teacher[t][1];
 			}
-			else if ((s >= 13 || s < 26) && !totaledHour(week, teacher, 2)) {
-				week[s] = teacher[t];
+			else if ((s >= 13 || s < 26) && !totaledHour(week, teacher[t][1], 2)) {
+				week[s] = teacher[t][1];
 			}
-			else if ((s >= 26 || s < 39) && !totaledHour(week, teacher, 3)) {
-				week[s] = teacher[t];
+			else if ((s >= 26 || s < 39) && !totaledHour(week, teacher[t][1], 3)) {
+				week[s] = teacher[t][1];
 			}
-			else if ((s >= 39 || s < 52) && !totaledHour(week, teacher, 4)) {
-				week[s] = teacher[t];
+			else if ((s >= 39 || s < 52) && !totaledHour(week, teacher[t][1], 4)) {
+				week[s] = teacher[t][1];
 			}
-			else if ( s >= 52 && !totaledHour(week, teacher, 5)) {
-				week[s] = teacher[t];
+			else if ( s >= 52 && !totaledHour(week, teacher[t][1], 5)) {
+				week[s] = teacher[t][1];
 			}
+
 		}
 	}
+}
+
+bool Subject::totaledHour(int(&subjectWeek)[65], int teacher, int day, int teacherArray[18][2])
+{
+	switch (day) {
+	case 1:
+		for (int i = 0; i < 13; i++) {
+			if (subjectWeek[i] == teacher)
+				
+		}
+	case 2:
+		for (int i = 13; i < 26; i++) {
+
+		}
+	case 3:
+		for (int i = 26; i < 39; i++) {
+
+		}
+	case 4:
+		for (int i = 39; i < 52; i++) {
+
+		}
+	case 5:
+		for (int i = 52; i < 65; i++) {
+
+		}
+	}
+	return false;
 }
--- Application/C++_Sorting/PlacementAlgorithm/ConsoleApplication4/FillWeek.h
+++ Application/C++_Sorting/PlacementAlgorithm/ConsoleApplication4/FillWeek.h
@@ -6,16 +6,16 @@
 class Subject {
 protected:
 	int subjectWeek[65] = { 0 };
-	int teacherArray[18] = { 0 };
+	int teacherArray[18][2] = { {0}, {0} };
 	int day = 1;
 	
 
 public:
-	void populatedArrays(int subjectWeek[65], int teacherArray[18]);
+	void populatedArrays(int subjectWeek[65], int teacherArray[18][2]);
 	
-	void placeTeachers(int (&week)[65], int (&teacher)[18]);
+	void placeTeachers(int (&week)[65], int (&teacher)[18][2], int teacherArray[18][2]);
 
-	bool totaledHour(int (&subjectWeek)[65], int (&teacherArray)[18], int day);
+	bool totaledHour(int (&subjectWeek)[65], int teacher, int day, int teacherArray[18][2]);
 
 	void beginPlacement();
 };
