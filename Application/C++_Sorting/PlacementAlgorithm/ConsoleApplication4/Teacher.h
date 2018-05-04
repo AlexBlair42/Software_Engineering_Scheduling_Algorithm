62746922aee49d3101f49b58f01b9c4faedc0b12
Alexander J Blair <ablair18@my.whitworth.edu>
2018-05-04 00:07:16 -0700

    Finished for the night.

--- Application/C++_Sorting/PlacementAlgorithm/ConsoleApplication4/Teacher.h
+++ Application/C++_Sorting/PlacementAlgorithm/ConsoleApplication4/Teacher.h
@@ -1,5 +1,15 @@
 #pragma once
 #include <string>
+
+
+
+// Website for factory Methods
+//https://www.geeksforgeeks.org/design-patterns-set-2-factory-method/
+enum Teacher_Type
+{
+	Kindergarten, First_Grade, Second_Grade, Third_Grade, Fourth_Grade, Fifth_Grade, Sixth_Grade
+};
+
 /*
 Parent class for teacher objects
 */
@@ -15,13 +25,17 @@ protected:
 
 public:
 
+	virtual std::string Get_Name() { return Name; }
+
+	virtual void Set_Name(std::string nm){}
+
 	virtual bool Get_Specialist_Status() { return Is_in_Specialist; }
 
 	virtual void Set_Grade_Level(int gl) {}
 
 	virtual int Get_Grade_Level() { return 0; }
 
-	Teacher(){}
+	static Teacher* Create(Teacher_Type);
 
 	Teacher(int Grade_Level, bool Is_In_Specialist, std::string Name) 
 	{
