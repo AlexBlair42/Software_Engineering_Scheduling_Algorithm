545aed981f2b7f9ef498c5757ae724977242a3c8
cblair-garcia18 <cblair-garcia18@my.whitworth.edu>
2018-05-17 20:59:13 -0700

    added constraints

--- Application/RootProject/Schedule_Application/Schedule_Application/FirstGrade_T.h
+++ Application/RootProject/Schedule_Application/Schedule_Application/FirstGrade_T.h
@@ -6,4 +6,15 @@ class FirstGrade_T : public Teacher
 {
 public:
 	void print_teacher_info();
+
+
+	const int lunchmin = 40;
+
+	bool litfitreq = 0;
+
+	bool physstatus = 0;
+	bool lunchstatus = 0;
+	bool libstatus = 0;
+	bool musicstatus = 0;
+	bool socstatus = 0;
 };
